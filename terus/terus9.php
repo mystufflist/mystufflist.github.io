129fa0d67ae824c8ebfb2cc5a5f2a388592b4b31
YoshiGamer123 <3dsgames69@gmail.com>
2024-06-21 03:40:10 -0500

    Update terus9.php

--- terus/terus9.php
+++ terus/terus9.php
@@ -1,20 +1,21 @@
 var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
 if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
 {
-  let window = _____WB$wombat$assign$function_____("window");
-  let self = _____WB$wombat$assign$function_____("self");
-  let document = _____WB$wombat$assign$function_____("document");
-  let location = _____WB$wombat$assign$function_____("location");
-  let top = _____WB$wombat$assign$function_____("top");
-  let parent = _____WB$wombat$assign$function_____("parent");
-  let frames = _____WB$wombat$assign$function_____("frames");
-  let opener = _____WB$wombat$assign$function_____("opener");
+ 
+# let window = _____WB$wombat$assign$function_____("window");
+ # let self = _____WB$wombat$assign$function_____("self");
+  #let document = _____WB$wombat$assign$function_____("document");
+  #let location = _____WB$wombat$assign$function_____("location");
+  #let top = _____WB$wombat$assign$function_____("top");
+  #let parent = _____WB$wombat$assign$function_____("parent");
+  #let frames = _____WB$wombat$assign$function_____("frames");
+  #let opener = _____WB$wombat$assign$function_____("opener");
 
-    if (window.self !== window.top) {
-        window.stop();
-    } else {
-        window.location.href = "https://smlwiki.com/";
-    }
+    #if (window.self !== window.top) {
+    #    window.stop();
+    #} else {
+    #    window.location.href = "https://smlwiki.com/";
+    #}
 
     document.getElementById("passwordInput").addEventListener("input", function() { this.value = this.value.toUpperCase(); });
     document.getElementById("passwordInput").addEventListener("keypress", function(event) {
